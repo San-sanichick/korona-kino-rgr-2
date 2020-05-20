83b4ad9a4337a1a216990f4a356afe895d8379c5
Alexander <sansanichick@gmail.com>
2020-05-20 11:46:24 +1000

    icon files, work on footer, fixing buttons

--- js/scrollUp.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", () => {
-    let scrollButton = document.getElementsByClassName("scroll-up")[0];
-    scrollButton.addEventListener("click", () => {
-        window.scrollTo(0,0);
-    }, false);
-}, true);
