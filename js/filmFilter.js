9c117b27fa7b3f57231919c75e8de714e6951bd0
Alexander <sansanichick@gmail.com>
2020-06-08 18:00:14 +1000

    main page done

---
+++ js/filmFilter.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () => {
+    let options = Array.from(document.getElementsByClassName("option"));
+    console.log(options);
+
+    options.forEach((o) => {
+        o.addEventListener("click", (e) => {
+            options.forEach((el) => {
+                el.classList.toggle("option-highlighted");
+            });
+        }, false);
+    });
+
+}, true);
